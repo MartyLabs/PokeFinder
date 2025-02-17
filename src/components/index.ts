34f8b83c1a8e9ff5d5df57ed80c6600222b57e84
arthur.mariet@epitech.eu <arthur.mariet@epitech.eu>
2025-02-17 15:28:11 +0100

    feat: Global component export file

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Spinner } from "./UI/Spinner";
+export { default as SearchBar } from "./UI/SearchBar";
+export { default as PokeCard } from "./Pokemon/PokeCard";
+export { default as PokeDetails } from "./Pokemon/PokeDetails";
